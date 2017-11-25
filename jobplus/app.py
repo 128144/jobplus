09380abd20e49cbe297ee5d936bf4999853bb72c
128144 <128144@163.com>
2017-11-25 17:57:04 +0800

    增加 macros.html, 修改forms.py,app.py,login.html

--- jobplus/app.py
+++ jobplus/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from jobplus.config import configs
 from jobplus.models import db
+from flask_migrate import Migrate
 
 def register_blueprints(app):
     from .handlers import front, admin, company, job, user
@@ -15,6 +16,7 @@ def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
     db.init_app(app)
+    Migrate(app, db)
     register_blueprints(app)
 
     return app
