d2eb9259d3a45e2b96c4880ac043370421a4e4a6
128144 <128144@163.com>
2017-11-27 22:30:58 +0800

    加入job.html和company.html

--- jobplus/handlers/company.py
+++ jobplus/handlers/company.py
@@ -5,7 +5,14 @@ from flask_login import login_user, logout_user, login_required
 
 company = Blueprint('company', __name__, url_prefix='/company')
 
+@company.route('/')
+def index():
+    return render_template('company.html')
+
+
 @company.route('/admin/profile', methods=['GET', 'POST'])
 def profile():
     form = CompanyprofileForm()
     return render_template('company/admin/profile.html', form=form)
+
+
--- jobplus/handlers/job.py
+++ jobplus/handlers/job.py
@@ -2,4 +2,6 @@ from flask import Blueprint, render_template
 
 job = Blueprint('job', __name__, url_prefix='/job')
 
-
+@job.route('/')
+def index():
+    return render_template('job.html')
